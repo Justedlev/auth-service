04c128ba48c424d54c657f7d1b3639fade84aa0a
Edward L <59051661+Justedlev@users.noreply.github.com>
2022-12-16 19:33:03 +0200

    -

--- auth-client/src/main/java/com/justedlev/auth/client/configuration/AuthFeignClientConfiguration.java
+++ auth-client/src/main/java/com/justedlev/auth/client/configuration/AuthFeignClientConfiguration.java
@@ -1,20 +1,24 @@
 package com.justedlev.auth.client.configuration;
 
 import feign.Request;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.TimeUnit;
 
 @Configuration
+@RequiredArgsConstructor
 public class AuthFeignClientConfiguration {
+    private final AuthFeignClientProperties properties;
+
     @Bean
     public Request.Options requestOptions() {
         return new Request.Options(
-                10,
-                TimeUnit.SECONDS,
-                1,
-                TimeUnit.MINUTES,
+                properties.getConnectTimeout().toMillis(),
+                TimeUnit.MILLISECONDS,
+                properties.getReadTimeout().toMillis(),
+                TimeUnit.MILLISECONDS,
                 Boolean.FALSE
         );
     }
